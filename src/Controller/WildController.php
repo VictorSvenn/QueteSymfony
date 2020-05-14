284ef9886dc9d12f96c8268a928985bfd550883b
Victor <victorsvenn@hotmail.com>
2020-05-14 10:30:29 +0200

    Quête 5 Symfony

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -7,15 +7,36 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * Class WildController
+ * @package App\Controller
+ * @Route("wild", name="wild")
+ */
 class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("", name="wild_index")
      */
-    public function index() :Response
+    public function index(): Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
+        ]);
+    }
+
+    /**
+     * @Route("/show/{slug}",
+     * defaults={"slug"= "Aucune série sélectionnée, veuillez choisir une série"},
+     * requirements={"slug"= "[a-z0-9-]+"},
+     * name="wild_show")
+     */
+    public function show(string $slug): Response
+    {
+        $slug = str_replace("-", ' ', $slug);
+        $slug = ucwords($slug);
+        return $this->render('wild/show.html.twig', [
+            'slug' => $slug,
         ]);
     }
 }
